0f79c791a5ff8f7b111a69675995813393879b78
Kasuhi <30944254+Kasuhi@users.noreply.github.com>
2020-03-22 17:36:29 +0530

    profile model done

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const app = express();
 //Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 50
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
