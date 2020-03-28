7896a9cccdfd654917e14e5d7f21970b759e1386
Kasuhi <30944254+Kasuhi@users.noreply.github.com>
2020-03-28 19:56:35 +0530

    redux setup + alert system done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 //redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,14 +16,15 @@ const App = () => (
     <Router>
       <Fragment>
         <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
 
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
+          <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
-          </section>
-        </Switch>
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   </Provider>
