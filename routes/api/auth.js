c51dd24e8089d9fa3aeea392be89fccac5ff3586
Kasuhi <30944254+Kasuhi@users.noreply.github.com>
2020-03-29 20:13:28 +0530

    react user authentication start

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -32,7 +32,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
--- routes/api/users.js
+++ routes/api/users.js
@@ -28,7 +28,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
