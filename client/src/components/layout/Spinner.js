bb1de10cb8e2a98a84a1a5419594980e3d4fd931
Kasuhi <30944254+Kasuhi@users.noreply.github.com>
2020-04-01 01:22:14 +0530

    dashboard started

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+
+export default () => (
+  <Fragment>
+    <iframe
+      src='https://gfycat.com/ifr/ConventionalOblongFairybluebird'
+      frameborder='0'
+      scrolling='no'
+      allowfullscreen
+      width='640'
+      height='684'
+    ></iframe>
+    <p>
+      {' '}
+      <a href='https://gfycat.com/conventionaloblongfairybluebird'>
+        via Gfycat
+      </a>
+    </p>
+  </Fragment>
+);
