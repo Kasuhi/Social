c26e912bba2c69441e650fc6540584bc0a286be0
Kasuhi <30944254+Kasuhi@users.noreply.github.com>
2020-04-06 21:13:52 +0530

    posts start

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,22 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPosts } from '../../actions/post';
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return <div></div>;
+};
+
+Posts.propTypes = {};
+
+const mapStateToProps = (state) => ({
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
